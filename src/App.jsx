c584932aa603ec29fb8e10ee53c62abad725a5b8
Quang Nguyen <quanguyendg@gmail.com>
2023-03-10 23:56:55 -0600

    add form component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import BaristaForm from './components/baristaForm'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <div className='title-container'>
+        <h1 className='title'>On My Grind</h1>
+        <p>So you hink you can barista? Let's put that to the test...</p>
+      </div>
+      <BaristaForm />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/baristaForm.jsx
@@ -0,0 +1,11 @@
+import React, {Component, useState} from "react";
+const BaristaForm = () => {
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default BaristaForm;
