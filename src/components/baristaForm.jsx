1c43da6f419f5141e0511b9007e37a427476860c
Quang Nguyen <quanguyendg@gmail.com>
2023-03-13 00:16:14 -0500

    add form

--- src/components/baristaForm.jsx
+++ src/components/baristaForm.jsx
@@ -3,7 +3,23 @@ const BaristaForm = () => {
 
   return (
     <div>
+      <form>
 
+        // we will fill this in soon!
+
+      </form>
+
+      <button type="submit" className="button submit" onClick={onCheckAnswer}>
+        Check Answer
+      </button>
+
+      <button
+        type="new-drink-button"
+        className="button newdrink"
+        onClick={onNewDrink}
+      >
+        New Drink
+      </button>
     </div>
   );
 }
